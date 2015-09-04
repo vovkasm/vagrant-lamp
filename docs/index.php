ae8c315a046137c3a055b585861d367b9323f53e
Vladimir Timofeev <vovkasm@gmail.com>
2015-09-04 17:22:01 +0300

    Sample index.php

---
+++ docs/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html><head>
+    <title>LAMP server</title>
+</head>
+<body>
+<h1>The LAMP Server</h1>
+<h2>phpmyadmin</h2>
+The phpmyadmin is <a href="/phpmyadmin">here</a>.
+<h2>databases</h2>
+Root mysql user/password: root/root.
+<h2>phpinfo</h2>
+<?php phpinfo(); ?>
